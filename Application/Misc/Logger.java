4b76077f99ab220e844bf955efd06a07c16427dd
Adam Svetec <adamsvetec@gmail.com>
2016-03-29 22:42:48 -0400

    Add a logger for errors

---
+++ Application/Misc/Logger.java
@@ -0,0 +1,11 @@
+//Adam Svetec
+//CSE 241
+
+//Phone object that will be intermediary between user interface and database phone object
+public class Logger{
+
+	//Populates database with random data
+	public static void logError(String error){
+		System.out.println("Error: "+error);
+	}
+}
