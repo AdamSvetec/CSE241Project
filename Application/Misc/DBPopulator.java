92fc652c8f42b63eba7e0c4b552bdb66c8e83d47
Adam Svetec <adamsvetec@gmail.com>
2016-03-31 09:43:38 -0400

    Add DBPopulator class that will populate the database if run_script is given an argument of populate

---
+++ Application/Misc/DBPopulator.java
@@ -0,0 +1,61 @@
+//Adam Svetec
+//CSE 241
+
+//DBPopulator will call populate DB methods of all classes in order to create random data
+public class DBPopulator{
+
+	//Clear all entities and repopulate them
+	public static void main(String [] args){
+		JogWireless.validateUser();
+		clearDB();
+		populateDB();
+	}
+
+	//Deletes all instances from database
+	private static void clearDB(){
+		PhoneNumber.deleteAll();
+		System.out.println("deleted phone_number");
+		InternetAccess.deleteAll();
+		System.out.println("deleted internet_access");
+		PhoneCall.deleteAll();
+		System.out.println("deleted phone_call");
+		TextMessage.deleteAll();
+		System.out.println("deleted text_message");
+		InventoryItem.deleteAll();
+		System.out.println("deleted inventory_item");
+		Account.deleteAll();
+		System.out.println("deleted account");
+		Phone.deleteAll();
+		System.out.println("deleted phone");
+		Store.deleteAll();
+		System.out.println("deleted store");
+		Customer.deleteAll();
+		System.out.println("deleted customer");
+		Plan.deleteAll();
+		System.out.println("deleted plan");
+	}
+
+	//Populates database with random but realistic entities from each class
+	private static void populateDB(){
+		Phone.populateDB();
+		System.out.println("phone populated");
+		PhoneCall.populateDB();
+		System.out.println("phone_call populated");
+		TextMessage.populateDB();
+		System.out.println("text_message populated");
+		InternetAccess.populateDB();
+		System.out.println("internet_access populated");
+		Store.populateDB();
+		System.out.println("store populated");
+		InventoryItem.populateDB();
+		System.out.println("inventory_item populated");
+		Customer.populateDB();
+		System.out.println("customer populated");
+		Plan.populateDB();
+		System.out.println("plan populated");
+		Account.populateDB();
+		System.out.println("account populated");
+		PhoneNumber.populateDB();
+		System.out.println("phone_number populated");
+	}
+}
--- Application/run_script.sh
+++ Application/run_script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-java -cp Output/:Output/ojdbc6.jar JogWireless
+if [ $# -ge 1 ] && [ $1 == "populate" ]; then
+	set -x
+	java -cp Output/:Output/ojdbc6.jar DBPopulator
+elif [ $# -ge 1 ]; then
+	echo "parameter" "\""$1"\"" "not recognized"
+else
+    set -x
+    java -cp Output/:Output/ojdbc6.jar JogWireless
+fi
