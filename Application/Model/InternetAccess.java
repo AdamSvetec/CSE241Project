3b7be58073c137edfc6266b58139706fa54cff56
Adam Svetec <adamsvetec@gmail.com>
2016-04-12 23:20:31 -0400

    Fix insertion bug in InternetAccess

--- Application/Model/InternetAccess.java
+++ Application/Model/InternetAccess.java
@@ -47,7 +47,7 @@ public class InternetAccess{
 			return -1;
 		}
 		String query = "insert into internet_access values ( '"+id+"', '"+meid+"', "+DateFormatter.toString(usageTime)+", '"+accessBytes+"')";
-		if(DBConnection.submitQuery(query)){
+		if(!DBConnection.submitQuery(query)){
 			this.id = -1;
 		}
 		return this.id;
