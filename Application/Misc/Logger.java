2c803606faa4e2e98e963562ab5422242975badf
Adam Svetec <adamsvetec@gmail.com>
2016-05-01 12:39:03 -0400

    Add File output to Logger so that errors are appended to log file

--- Application/Misc/Logger.java
+++ Application/Misc/Logger.java
@@ -1,11 +1,26 @@
 //Adam Svetec
 //CSE 241
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.StandardOpenOption;
+import java.util.Date;
+
 //Logger object that will be called to output errors to whichever medium desired
 public class Logger{
 
 	//Populates database with random data
 	public static void logError(String error){
 		System.out.println("Error: "+error);
+
+		try {
+			File yourFile = new File("RuntimeLog.txt");
+    		yourFile.createNewFile();
+            Files.write(Paths.get(yourFile.getPath()), (""+new Date()+" : "+error+"\n").getBytes(), StandardOpenOption.APPEND);
+        }catch (IOException ioe) {
+            System.out.println("Error: logging failed");
+        }
 	}
 }
