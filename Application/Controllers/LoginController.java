50e7a1d7674ed80ba07f6b3e7c5b56629525cdbb
Adam Svetec <adamsvetec@gmail.com>
2016-05-01 20:53:59 -0400

    Specify oracle login

--- Application/Controllers/LoginController.java
+++ Application/Controllers/LoginController.java
@@ -11,8 +11,8 @@ public class LoginController{
   	//Attempt to login user into the application
   	private void login(){
  		do{
-  			String username = CommandLineView.getString("Please enter username: ");
-			String password = CommandLineView.getString("Please enter password: ");
+  			String username = CommandLineView.getString("Please enter oracle username: ");
+			String password = CommandLineView.getString("Please enter oracle password: ");
 			if(DBConnection.validate(username, password)){
 				new InterfaceSelectController();
 				return;
