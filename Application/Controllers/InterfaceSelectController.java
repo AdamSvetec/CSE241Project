0f53b2f6233f8d897086124f851b29a786ae2c40
Adam Svetec <adamsvetec@gmail.com>
2016-05-05 14:26:27 -0400

    Add exit option to git interface selector

--- Application/Controllers/InterfaceSelectController.java
+++ Application/Controllers/InterfaceSelectController.java
@@ -9,13 +9,15 @@ public class InterfaceSelectController{
 
     //Gets the user to select the interface they would like to use
     public void selectInterface(){
-    	int interfaceChoice = CommandLineView.getInt("Please enter interface you would like to explore: \n\t1 : Interactive\n\t-Employee handles requests from customer in store\n\t2 : Streaming\n\t-Employee reads in usage record file\n");
+    	int interfaceChoice = CommandLineView.getInt("Please enter interface you would like to explore: \n\t1 : Interactive\n\t-Employee handles requests from customer in store\n\t2 : Streaming\n\t-Employee reads in usage record file\n\t3 : Exit\n");
     	while(true){
     		if(interfaceChoice == 1){
     			new InteractiveController();
     		}else if(interfaceChoice == 2){
     			new UsageReaderController();
-    		}else{
+    		}else if(interfaceChoice == 3){
+		        return;
+		}else{
                 System.out.println("Error: did not recognize choice, please try again");
     			selectInterface();
     		}
